87fd1fe3144d6aa4313eb4e962e8f40f7e658592
Nava <navajay2018@gmail.com>
2025-04-29 16:57:32 +0330

    create classes and packages

---
+++ src/Products/Product.java
@@ -0,0 +1,44 @@
+package Products;
+
+public abstract class Product {
+    private String title;
+    private Double price;
+    private String id;
+
+     public Product(String title, Double price, String id){
+        this.title = title;
+        this.price = price;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title){
+         if(!title.isEmpty())
+             this.title = title;
+         else
+             System.out.println("Error: invalid title!");
+    }
+
+    public void setId(String id) {
+        int intID = Integer.parseInt(id);
+        this.id = id;
+    }
+
+    public void setPrice(Double price){
+         if(!(price < 0))
+             this.price = price;
+         else
+             System.out.println("Error: invalid price!");
+    }
+}
