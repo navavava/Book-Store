f52db80a4a934d197dd50363e4d995f7d82131a4
Nava <navajay2018@gmail.com>
2025-04-29 17:24:35 +0330

    add methods for Accessory

--- src/Products/Accessory.java
+++ src/Products/Accessory.java
@@ -1,12 +1,31 @@
 package Products;
 
-public class Accessory extends Product{
+public class Accessory extends Product {
+
+    private String color;
+
     public Accessory(String title, Double price, String id) {
         super(title, price, id);
     }
 
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        if (!color.isEmpty())
+            this.color = color;
+        else
+            System.out.println("Error: invalid color!");
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString() + ", Color: " + color);
+    }
+
     @Override
     protected String generateId() {
-        return "";
+        return "3";
     }
 }
